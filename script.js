09c4a3536bfba67b386f6df747e550b43d8d80b4
nickborbe <nickborbe@gmail.com>
2019-11-16 10:57:37 -0500

    wack mole

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('hello');
+
+window.onload = function(){
+
+
+
+
+    let mole = document.createElement('div');
+    mole.classList.add('mole');
+
+
+    
+    
+ 
+    console.log(document.getElementById('game-board'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// end window onload
