84b5bc12a887e75fd9a96c8db30ec4dad4441f09
Veronica <veronica1506@gmail.com>
2019-11-16 11:17:13 -0500

    Add random wack

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 console.log('hello');
 
-window.onload = function(){
-
-
-
+window.onload = function () {
 
     let mole = document.createElement('div');
     mole.classList.add('mole');
 
-
-    
-    
- 
     console.log(document.getElementById('game-board'))
 
+    //I need get each space
 
+    function randomMole(mole) {
+        let space = document.getElementsByClassName('col')
 
+        let randomPlace = Math.floor(Math.random() * space.length)
+        space[randomPlace].append(mole)
 
+    }
 
-
-
-
+    randomMole(mole)
 
 
 
